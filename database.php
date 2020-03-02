07c6e27556567c10726047addcaef1bdd7089766
joanne11 <32472603+joanne11@users.noreply.github.com>
2020-03-02 17:34:11 +0800

    modify the database connection

--- database.php
+++ database.php
@@ -1,11 +1,17 @@
-<?php
-	define ('SERVER', 'localhost');
-	define ('USER' , 'root');
-	define ('PASSWORD' , '');
-	define ('DATABASE' , 'e-learning');
-	
-  $conn = mysqli_connect('localhost', 'root', '', 'e-learning');
-	if (mysqli_connect_errno()) {
-		echo "<p>Connection failed:".mysqli_connect_error()."</p>\n";
-	}
+<?php //ob_start(); // Make sure you put this in line 1 with no space
+
+//session_start();
+//session_destroy();
+
+defined("DB_HOST") ? null : define("DB_HOST", "localhost");
+
+defined("DB_USER") ? null : define("DB_USER", "root");
+
+defined("DB_PASS") ? null : define("DB_PASS",  "");
+
+defined("DB_NAME") ? null : define("DB_NAME", "nightowls");
+
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+
 ?>
