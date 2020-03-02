93866eae7bbab52a60f9c5a61b5405e9cbf367e1
joanne11 <32472603+joanne11@users.noreply.github.com>
2020-03-02 17:40:16 +0800

    A navigation page is included

---
+++ include/navigation.php
@@ -0,0 +1,8 @@
+  <nav class="navbar navbar-light bg-light">
+    <span class="navbar-brand mb-0 h1">E-learning</span>
+    <ul class="nav justify-content-end">
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Admin</a>
+      </li>
+    </ul>
+  </nav>
