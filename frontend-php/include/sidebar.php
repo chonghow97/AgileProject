5c815c5d1725226de7c502c76fc9deef067e404d
TCH <chong.how97@live.com>
2020-03-13 17:31:12 +0800

    updated student_dashboard UI

--- frontend-php/include/sidebar.php
+++ frontend-php/include/sidebar.php
@@ -1,9 +1,6 @@
-<div class="list-group col-md-3 col-xs-9">
 
-  <a href="#" class="list-group-item list-group-item-action active">Dashboard</a>
-  <a href="#" class="list-group-item list-group-item-action">Message</a>
-  <a href="#" class="list-group-item list-group-item-action">Metting</a>
-  <a href="#" class="list-group-item list-group-item-action">FORUMS</a>
-  <a href="#" class="list-group-item list-group-item-action">UPLOADS</a>
-  <a href="#" class="list-group-item list-group-item-action">INBOX</a>
-</div>
+  <?php 
+  	foreach ($sidebar as $key => $value) {
+  		echo "<a href='#' class='list-group-item list-group-item-action'>$value</a>";
+  	}
+   ?>
--- frontend-php/student_dashboard.php
+++ frontend-php/student_dashboard.php
@@ -1,18 +1,151 @@
 <?php 
-	$title = "E-learning";
-	$name = "Student";
-	include './include/header.php';
+$title = "E-learning";
+$name = "Student";
+include './include/header.php';
+$sidebar = ['Dashboard','Message','Meetings','Forums','Files / Uploads','Inbox'];
+$active_index = 0;
 ?>
-<div class="container-sm row">
-<?php include 'sidebar.php'; ?>
-<div class="col-md-9">
-  <div class="row">
-    <?php include './include/sidebar.php'; ?>
-  </div>
-</div>
-</div>
-<?php 
+
+<div class="container-fluid row">
+	<div class="list-group col-3 mt-3">
+
+		<?php include './include/sidebar.php'; ?>
+		<div class="fixed-bottom">
+			<?php 
+				//lecture details
+				$L_name = "Stacy";
+				$L_Email = "stacy@gamil.com";
+				$L_PhoneNumber = "+601236152221";
+			 ?>
+			<li class="list-group-item active bg-dark btn-outline-dark"><label class="font-weight-bold pr-3 ">TUTOR'S DETAILS</li>
+				<?php 
+					if(0){
+						echo "
+							<li class='list-group-item'><label class='font-weight-bold pr-3'>Name:</label>$L_name</li>
+							<li class='list-group-item'><label class='font-weight-bold pr-3'>Email:</label>$L_Email</li>
+							<li class='list-group-item'><label class='font-weight-bold pr-3'>Subject:</label>$L_PhoneNumber</li>
+						";
+					}else{
+						echo "<li class='list-group-item'><label class='font-weight-bold pr-3'>Unassigned</li>";
+					}
+				 ?>
+			
+		</div>
+			
+		</div>
+
+
+		<div class="col-9">
+			<div class="row">
+				<!--Inbox -->
+				<div class="col mt-3">
+					<div class="card" style="width: 18rem;">
+						<div class="card-header font-weight-bold">
+							Inbox
+						</div>
+						<ul class="list-group list-group-flush">
+							<?php
+							$title = "Github";
+							$content = "Hello User..";
+							$date = "12FEB";
+							$Empty = "<li class='list-group-item font-weight-lighter font-italic text-center'>Empty</li>";
+							if(1){
+								for($x=0;$x<5;$x++){
+								echo "<li class='list-group-item'>
+								<label class='font-weight-bold'>$title</label>
+								<br>
+								<small>$content<label class='float-right font-weight-bold text-uppercase'>$date</label></small>
+								</li>";
+								}
+							}else{
+								echo $Empty;
+							} 
+							
+							?>
+						</ul>
+					</div>
+				</div>
+
+
+				<!--Files -->
+				<div class="col mt-3">
+					<div class="card" style="width: 18rem;">
+						<div class="card-header font-weight-bold ">
+							Files
+						</div>
+						<ul class="list-group list-group-flush">
+							<?php 
+								$file_name = "123.docx";
+								$date = "12FEB";
+								$url = "#";
+
+								if (1) {
+									for ($i=0; $i < 5; $i++) { 
+										echo "<li class='list-group-item'><a href='$url'>$file_name</a><b class='float-right'>$date</b></li>";
+									}
+								}else{
+									echo $Empty;
+								}
+								
+								
+							 ?>
+							
+						</ul>
+					</div>
+				</div>
+				<!--Meetings -->
+				<div class="col mt-3">
+					<div class="card" style="width: 18rem;">
+						<div class="card-header font-weight-bold">
+							Meetings
+						</div>
+						<ul class="list-group list-group-flush">
+
+							<?php 
+								if(1){
+									//pending
+									echo "<li class='list-group-item'>Cras justo odio <span class='float-right font-weight-bold font-italic'>Pending</span></li>";
+
+									//approved
+									echo "<li class='list-group-item'>Dapibus ac facilisis in <span class='float-right font-weight-bold font-italic text-success'>Approved</span></li>";
+									//declined
+									echo "<li class='list-group-item'>Vestibulum at eros <span class='float-right font-weight-bold font-italic text-danger'>Declined</span></li>";
+								}else{
+									//empty
+									echo $Empty;
+								}
+							 ?>
+
+							
+							
+						</ul>
+					</div>
+				</div>
+				<!--Forums -->
+				<div class="col mt-3">
+					<div class="card" style="width: 18rem;">
+						<div class="card-header font-weight-bold">
+							Forums
+						</div>
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item">Cras justo odio <span class="float-right font-italic">just now</span></li>
+							<li class="list-group-item">Cras justo odio <span class="float-right font-italic">just now</span></li>
+							<li class="list-group-item">Cras justo odio <span class="float-right font-italic">just now</span></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php 
 	include './include/footer.php';
- ?>
+	?>
+	<script type="text/javascript">
+		$(function () {
+			$(".list-group-item:nth(<?php echo $active_index ?>)").addClass("active bg-dark btn-outline-dark");
+		})
+	</script>
+
+
 
 
