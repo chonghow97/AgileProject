e6aa115fcfc2c1a983978d389ffd9cfd325a7167
Joanne <joanne11esther@gmail.com>
2020-04-03 10:02:29 +0800

    admin function is updated

--- Admin_dashboard.php
+++ Admin_dashboard.php
@@ -1,12 +1,16 @@
 <?php 
+
 $title = "E-learning";
-$name = "Admin";
-include './include/function.php';
-include './include/database.php';
 include './include/header.php';
 $sidebar = ['Allocation','List'];
-$url = ['./Admin_dashboard.php','./Admin_list.php'];
+$url = ['Admin_dashboard.php','Admin_list.php'];
 $active_index = 0;
+	// if ($_SESSION == null) {
+	// 	echo "getout";
+	// }else{
+	// 	var_dump($_SESSION);
+	// }
+
 ?>
 <style type="text/css">
 	*{
@@ -16,65 +20,47 @@ $active_index = 0;
 		resize: none;
 	}
 </style>
-<div class="container-fluid row">
-	<div class="list-group col-3">
 
-		<?php include './include/sidebar.php'; ?>
-
-
+<div class="row">
+	<div class="list-group col-2">
+		<?php include 'include/sidebar.php'; ?>
 	</div>
-	<div class="col-9">
+	<div class="col p-3">
+		<h3>Allocation</h3>
+		<hr>
+<?php allocate(); ?>
 
-		<div class="container p-3">
-			
-			<h3>Allocation</h3>
-			<hr>
-			<form action="Admin_dashboard.php" method="post">
-				<div class="form-group row">
-					<label for="staticEmail" class="col-sm-2 col-form-label">Tutor</label>
-					<div class="col-sm-10">
-						<select class="custom-select" id="tutor">
-							<option selected disabled value="">Select A Tutor</option>
-							<?php get_tutor(); ?>
-						</select>
+		<form action="" method="post">
+			<div class="form-group row">
+				<label for="staticEmail" class="col-sm-2 col-form-label">Tutor</label>
+				<div id="selectTutorOption" class="col-sm-10">
+					<select class="custom-select" name="select_option" id="tutor">
+						<option selected disabled value="">Select A Tutor</option>
+						<?php get_tutor(); ?>
+					</select>
+				</div>
+			</div>
+
+			<div class="form-group row ">
+				<label for="staticEmail" class="col-sm-2 col-form-label">Student</label>
+				<div class="col-sm-10">
+					<div class="p-3">
+						<input type="" name="" class="col-9" placeholder="Search" id="search">
+					</div>
+					<!-- checkbox -->
+					<div class="checkbox d-flex flex-wrap" id="student">
+						<?php 
+						get_student();
+						?>
+
 					</div>
 				</div>
-
-				<div class="form-group row ">
-					<label for="staticEmail" class="col-sm-2 col-form-label">Student</label>
-					<div class="col-sm-10">
-						<div class="p-3">
-							<input type="" name="" class="col-9" placeholder="Search" id="search">
-						</div>
-						<!-- checkbox -->
-						<div class="checkbox d-flex flex-wrap">
-							<?php 
-								get_student();
-							 ?>
-							
-						</div>
-					</div>
-				</div>
-				<div class="text-center">
-					<button type="button" class="btn btn-outline-dark col-3" data-toggle="modal" data-target="#finalCheck">Submit</button>
-				</div>
-				</form>
-				<fieldset disabled>
-				<div class="row">
-					<div class="text-center p-3 col-3">
-				<input type="text" id="tutorOut" class="form-control" placeholder="Lecture Name here">
 			</div>
-			<div class="text-center p-3 col-8">
-				<textarea class="form-control" id="studentOut" rows="5" placeholder="Student Name Here"></textarea>
+			<div class="text-center">
+				<button type="button" class="btn btn-outline-dark col-3" data-toggle="modal" data-target="#finalCheck">Submit</button>
 			</div>
-				</div>
+				<!-- Modal -->
 			
-			</fieldset>
-			</div>
-			<div id="result"></div>
-		</div>
-	</div>
-	<!-- Modal -->
 <div class="modal fade" id="finalCheck" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
@@ -89,27 +75,40 @@ $active_index = 0;
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-outline-dark" data-dismiss="modal">Let me think about it</button>
-        <button type="button" class="btn btn-secondary" id='allocate'>i'm sure</button>
+        <button type="button" name="allocate" class="btn btn-secondary" id='allocate'>I'm sure</button>
       </div>
     </div>
   </div>
 </div>
+		</form>
+		<fieldset disabled>
+			<div class="row">
+				<div class="text-center p-3 col-3">
+					<input type="text" name="tutor" id="tutorOut" class="form-control" placeholder="Lecture Name here">
+				</div>
+				<div class="text-center p-3 col-8">
+					<textarea class="form-control" name="student" id="studentOut" rows="5" placeholder="Student Name Here"></textarea>
+				</div>
+			</div>
+		</fieldset>
+	</div>
+</div>
+<?php 
+include './include/footer.php';
+?>
 
-	<?php 
-	include './include/footer.php';
-	?>
+<script type="text/javascript">
+	$(function () {
+		//apperance
+		$("nav:nth(0)").addClass("bg-dark");
+		$("nav:nth(0)>span").addClass("text-white");
 
-	<script type="text/javascript">
-		$(function () {
-			$(".alert").hide();
-			var TutorName = "";
-			//APPERENCE
-			$("#nav").addClass("bg-dark");
-			$(".list-group-item:nth(<?php echo $active_index ?>)").addClass("list-group-item-dark");
 
-			
-			//STUDENT NAME HERE
-			$(".checkbox,#tutor").change(function (){
+		$(".list-group-item:nth(<?php echo $active_index ?>)").addClass("list-group-item-dark");
+		var TutorName = "";
+
+		//checkbox verify
+		$(".checkbox,#tutor").change(function (){
 				TutorName = $("#tutor option:selected").text();
 				$("#tutorOut").attr("placeholder", TutorName);
 				var students = [];
@@ -124,30 +123,86 @@ $active_index = 0;
 				var content = "<table class='table table-sm'> <thead> <tr> <th scope='col'>Lecture</th> <th scope='col'>Student</th> </tr> </thead> <tbody> <tr> <td>"+TutorName+"</td> <td>"+students+"</td> </tr> </tbody> </table>";
 				finalContent.innerHTML = content;
 				//allocation
-				$("#allocate").click(function (){
+				$("#allocate").click(function (event){
 					//close modal
 					$(this).attr("data-dismiss","modal");
 
 					//post
-					
-				})
-			})
-			
-			//Search
-			$('#search').keyup(function (){
-				console.log($(this).val());
-			})
+					event.preventDefault();
+					if ($('#tutorOut').val() == '') 
+					{
+						alert("Please Select Tutor");
+						return false;
+					}
+					else if($('#studentOut').val() == '')
+					{
+						alert("Please Select Student");
+						return false;
+					}
+					else
+					{
+						$('#allocate').attr('disabled', 'disabled');
+						var form_data = $(this).serialize();
+						$.ajax({
+							url:"function.php",
+							method:"POST",
+							data:form_data,
+							success:function(data)
+							{
+								$('#allocate').attr("disabled", "disabled");
+								if(data == 'done')
+								{
+									$("#tutorOut").attr("placeholder", students);
+									$("#studentOut").attr("placeholder", students);
+									$('#allocate')[0].reset();
+									alert('Data Inserted');
+								}
+							}
+						});
+					}
+							
+						})
+					})
 
-			//test
-			$(".checkstudent").each(function (){
-				console.log($(this).children("label").text());
-			})
+		// $('#allocate').on('submit', function(event){
+		// 	event.preventDefault();
+		// 	if ($('#tutorOut').val() == '') 
+		// 	{
+		// 		alert("Please Select Tutor");
+		// 		return false;
+		// 	}
+		// 	else if($('#studentOut').val() == '')
+		// 	{
+		// 		alert("Please Select Student");
+		// 		return false;
+		// 	}
+		// 	else
+		// 	{
+		// 		$('#allocate').attr('disabled', 'disabled');
+		// 		var form_data = $(this).serialize();
+		// 		$.ajax({
+		// 			url:"function.php",
+		// 			method:"POST",
+		// 			data:form_data,
+		// 			success:function(data)
+		// 			{
+		// 				$('#allocate').attr("disabled", "disabled");
+		// 				if(data == 'done')
+		// 				{
+		// 					$('#student').html('');
+		// 					$('#tutorOut').html('');
+		// 					$('studentOut').html('');
+		// 					$('#allocate')[0].reset();
+		// 					alert('Data Inserted');
+		// 				}
+		// 			}
+		// 		});
+		// 	}
 
-			
+				
+		// });
 
-			//email part
+	});
+	
+</script>
 
-
-		});
-
-	</script>
--- admin_index.php
+++ admin_index.php
@@ -1,13 +1,30 @@
 <?php 
   $title = "E-learning";
   $name = "";
-  include './include/header.php';
+  include_once 'include/function.php';
+  include_once 'include/database.php';
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title><?php echo $title ?></title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+</style>
+
+<body class="container">
+  <header>
+      <nav class="navbar navbar-light">
+        <span class="navbar-brand mb-0 h1">E-learning</span>
+      </nav>
+  </header>
 <style type="text/css">
   *{
     //outline: 1px solid red;
-  }
-  .vertical-text {
+  } 
+  .vertical-text { 
 transform: rotate(90deg);
 transform-origin: left top 0;
 margin-left: 40px;
@@ -15,22 +32,22 @@ margin-left: 40px;
 </style>
 <!-- Sidebar -->
   <div class="float-left vertical-text p-1">
-  <a href="./" class="btn btn-primary">Student</a>
-  <a href="./admin_index.php" class="btn btn-secondary ml-3">Admin</a>
-  <a href="./lecture_index.php" class="btn btn-warning ml-3">Lecture</a>
+  <a href="index.php" class="btn btn-primary">Student</a>
+  <a href="Admin_index.php" class="btn btn-secondary ml-3">Admin</a>
+  <a href="Lecture_index.php" class="btn btn-warning ml-3">Lecture</a>
 </div>
 <div class="container-md p-3 text-center mt-5">
 <img src="https://dummyimage.com/200x200/343a40/fff.png&text=Logo+Here" alt="..." class="img-thumbnail">
 <h1 class="p-3">E-learning</h1>
 
-  <form class="p-3 border text-left col-5 mx-auto" action="#">
+  <form class="p-3 border text-left col-5 mx-auto" action="#" method="post">
   <?php admin_login(); ?>
     <div class="form-group">
-    <input name="email" type="email" placeholder="Enter Email address" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input name="email" type="email" placeholder="Enter Email address" class="form-control" id="" aria-describedby="emailHelp">
     <!-- <small class="text-danger">Invalid Username. Please Check Again!</small> -->
   </div>
   <div class="form-group">
-    <input name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter Password">
+    <input name="password" type="password" class="form-control" id="" placeholder="Enter Password">
     <!-- <small class="text-danger">Wrong Password. Please check again</small> -->
   </div>
   <div class="form-group form-check">
