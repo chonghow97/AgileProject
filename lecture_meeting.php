ea893db1e618039ce171fd977e48e498d69eaa61
Tan ChongHow <chong.how97@live.com>
2020-04-20 01:31:52 +0800

    -dash- Colour coding with the title for appointments

--- lecture_meeting.php
+++ lecture_meeting.php
@@ -14,6 +14,9 @@ $active_index = 2;
 	#studentOut{
 		resize: none;
 	}
+    td{
+        color: #3d3d3d;
+    }
 </style>
 <div class="container-fluid row">
 	<div class="list-group col-3">
@@ -22,25 +25,19 @@ $active_index = 2;
 
 
 	</div>  
-			<div class="col-9 row p-3">
+			<div class="col-9 row p-3 mx-auto">
 				<div class="col-lg-9">
 					<h3 class="p-3">Meeting Approvement</h3>
-
-<table class="table col-10 mx-auto table-bordered table-hover">
+<div class="overflow-auto mx-auto">
+<table class="table table-bordered table-hover overflow-auto">
     <thead>
         <tr>
             <th>Id</th>
             <th>Username</th>
             <th>Title</th>
             <th>Comment</th>
-            <th>Type of Meeting</th>
             <th>Venue</th>
-            <th>Time</th>
-            <th>Date</th>
-            <th>Status</th>
-            <th>Approve</th>
-            <th>Disapprove</th>
-            <th>Delete</th>
+            <th>Action</th>
         </tr>
     </thead>
     <tbody>
@@ -63,19 +60,21 @@ confirm($query);
     $date = date('d M Y', $date);
     $status = $row['status'];
 
-echo "<tr>";
+if($status == 'Approved'){
+    echo '<tr style="background-color:#03fcc2">';
+}elseif($status == 'Disapproved'){
+    echo '<tr style="background-color:#de5d93">';
+}
+else{
+    echo '<tr>';
+}
 echo "<td>$appointment_id</td>";
-echo "<td>$username</td>";
+echo "<td><b>$username<b></td>";
 echo "<td>$title</td>";
 echo "<td>$comment</td>";
-echo "<td>$meeting_type</td>";
-echo "<td>$venue</td>";
-echo "<td>$time</td>";
-echo "<td>$date</td>";
-echo "<td>$status</td>";
-echo "<td><a href='lecture_meeting.php?approve=$appointment_id' class='btn btn-outline-success' onclick='check_approve()'>Approve</a></td>";
-echo "<td><a href='lecture_meeting.php?disapprove=$appointment_id' class='btn btn-outline-warning' onclick='check_disapprove()'>Disapprove</a></td>";
-echo "<td><a href='lecture_meeting.php?delete=$appointment_id' class='btn btn-outline-danger' onclick='check_delete()'>Delete</a></td>";
+echo "<td><b>$venue ($meeting_type)</b><br> $time <br> $date</td>";
+echo "<td style='width: 110px'><div class='row mx-auto'><a href='lecture_meeting.php?approve=$appointment_id' class='btn btn-outline-dark btn-sm' onclick='check_approve()'>✔️</a>
+<a href='lecture_meeting.php?disapprove=$appointment_id' class='btn btn-outline-dark btn-sm ml-1' onclick='check_disapprove()'>❌</a></div></td>";
 echo "</tr>";
 
 }
@@ -86,7 +85,7 @@ echo "</tr>";
     </tbody>
 
 </table>
-
+</div>
 <?php 
 
 if(isset($_GET['approve'])) {
@@ -109,14 +108,7 @@ if(isset($_GET['disapprove'])) {
 }
 
 
-if(isset($_GET['delete'])) {
 
-    $delete_appointment_id = $_GET['delete'];
-    
-    $query = "DELETE FROM appointment WHERE appointment_id = {$delete_appointment_id} ";
-    $delete_query = mysqli_query($connection, $query);
-    header("Location: lecture_meeting.php");
-}
 
 
  ?>
@@ -138,24 +130,17 @@ if(isset($_GET['delete'])) {
 			//APPERENCE
 			$("#nav").addClass("bg-warning");
 			$(".list-group-item:nth(<?php echo $active_index ?>)").addClass("list-group-item-warning");
-		});
+            function check_approve() {
+            alert("Meeting status approve confirmed!");
+        }
 
-	</script>
 
-<script type="text/javascript">
+        function check_disapprove() {
+            alert("Meeting status disapprove confirmed!");
+        }
+        });
+
 	
-function check_delete() {
-    alert("Delete confirmed!");
-  }
-
-
-function check_approve() {
-    alert("Meeting status approve confirmed!");
-  }
-
-
-function check_disapprove() {
-    alert("Meeting status disapprove confirmed!");
-  }
+        
 
 </script>
